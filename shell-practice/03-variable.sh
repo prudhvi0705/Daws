edaf432723e999d2f156ef724588078eb735f0ef
prudhvi75 <m.vprudhvi7@gmail.com>
2025-10-28 16:45:28 +0530

    correcting 03 shell script

--- shell-practice/03-variable.sh
+++ shell-practice/03-variable.sh
@@ -2,7 +2,7 @@
 
 Person1=Suresh
 Person2=Ramesh
-echo "Person1 : Hi, Person2, How are you?"
-echo "Person2 : Hi, Person1, I am good how are you?"
-echo "Person1 : I am doing good. What are you learning? Person2"
-echo "Person2 : I am learning shell scripting Sresh"
+echo "$Person1 : Hi, $Person2, How are you?"
+echo "$Person2 : Hi, $Person1, I am good how are you?"
+echo "$Person1 : I am doing good. What are you learning? $Person2"
+echo "$Person2 : I am learning shell scripting $Person1"
