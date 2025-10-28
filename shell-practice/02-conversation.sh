e81449eccd04640bd04ece5dc23bd4dd60656632
prudhvi75 <m.vprudhvi7@gmail.com>
2025-10-28 16:38:01 +0530

    adding shell script

---
+++ shell-practice/02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Suresh : Hi, Ramesh, How are you?"
+echo "Ramesh : Hi, Suresh, I am good how are you?"
+echo "Suresh : I am doing good. What are you learning? Ramesh"
+echo "Ramesh : I am learning shell scripting Sresh"
