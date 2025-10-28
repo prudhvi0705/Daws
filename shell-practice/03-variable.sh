b49111679076bb3f9d44a30f58e86bae17f22392
prudhvi75 <m.vprudhvi7@gmail.com>
2025-10-28 16:40:14 +0530

    adding shell script

---
+++ shell-practice/03-variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+Person1 = Suresh
+Person2 = Ramesh
+echo "Person1 : Hi, Person2, How are you?"
+echo "Person2 : Hi, Person1, I am good how are you?"
+echo "Person1 : I am doing good. What are you learning? Person2"
+echo "Person2 : I am learning shell scripting Sresh"
