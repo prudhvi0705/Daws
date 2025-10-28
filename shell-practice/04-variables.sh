1662ed4e81b2b52262eb604b506c526ed2d59645
prudhvi75 <m.vprudhvi7@gmail.com>
2025-10-28 18:27:23 +0530

    modifying 4th script

--- shell-practice/04-variables.sh
+++ shell-practice/04-variables.sh
@@ -6,4 +6,4 @@ Person2=$2
 echo "$Person1 :: Hey $Person2, Welcome to chicago"
 echo "$Person2 :: Thank you $Person1. Finally Not on zoom"
 echo "$Person1 :: So we have a deal?"
-echo "$Person2 :: Yes $Person1, if make Ukraine joins Russia"
+echo "$Person2 :: Yes $Person1, if you make Ukraine joins Russia"
