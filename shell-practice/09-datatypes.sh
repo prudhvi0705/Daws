3344a77a4c61b55c20e9783d4853583d4f5626d4
prudhvi75 <m.vprudhvi7@gmail.com>
2025-10-29 13:43:38 +0530

    Adding 9th script for data types

---
+++ shell-practice/09-datatypes.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+NUMBER1=100
+NUMBER2=200
+NAME=MODI
+
+SUM=$((NUMBER1+NUMBER2+NAME))
+
+echo "Total sum is $SUM"
+
+Leaders=("NAMO" "TRUMP" "PUTIN" "TREDEAU")
+
+echo "First Leader is : ${Leaders[0]}"
