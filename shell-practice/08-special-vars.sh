b4d385e7c89bdce48d25b2d82acb16a07b15a26e
prudhvi75 <m.vprudhvi7@gmail.com>
2025-10-29 12:44:00 +0530

    Adding 8th Script for special variables

--- shell-practice/08-special-vars.sh
+++ shell-practice/08-special-vars.sh
@@ -4,7 +4,7 @@ echo "Print all args : $@"
 echo "Print all args : $*"
 echo "Script Name : $0"
 echo "Current working Directory : $PWD"
-echo "who is running this script : $USER"
+echo "who is running this script : $W"
 echo "Home directory of the user : $HOME"
 echo "PID of the current script : $$"
 sleep 50 &
