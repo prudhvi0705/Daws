2224a3a8177458af7f86f67b5238c7eb19c64f1d
prudhvi75 <m.vprudhvi7@gmail.com>
2025-10-29 14:58:12 +0530

    Adding 10th script for data types

--- shell-practice/10-conditions.sh
+++ shell-practice/10-conditions.sh
@@ -2,9 +2,9 @@
 
 NUMBER=$1
 
-if [ $NUMBER -eq 10] then;
+if [ $NUMBER -eq 10]; then
     echo "Given number $NUMBER is equal to 10"
-elif [ $NUMBER -gt 10 ] then;
+elif [ $NUMBER -gt 10 ]; then
     echo "Given number $NUMBER is greater than 10"
 else [ $NUMBER -lt 10]
     echo "Given number $NUMBER is less than 10"
